aed75c7a811d1f2a9b3e910283ed7407c2751e71
ellazmina <azmina4@gmail.com>
2020-10-30 01:45:46 +0600

    navbar and  title done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MyCarousel from './components/Carousel/MyCarousel';
+import Footer from './components/Footer/Footer';
+import MyNavbar from './components/Navbar/MyNavbar';
+import TitleMessage from './components/TitleMessage/TitleMessage';
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyNavbar></MyNavbar>
+      <MyCarousel></MyCarousel>
+      <TitleMessage></TitleMessage>
+      <Footer></Footer>
     </div>
   );
 }
